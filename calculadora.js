6bf96c29ce36900b6e2646477d7cf7ec9ad2ef7a
Felix Montes <hugomontes927@gmail.com>
2017-01-06 12:42:14 -0400

    Funcionalidad para el boton sumar

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,7 @@
 alert('Bienvenido a mi calculadora...');
+function sumar(){
+	var a = document.getElementById("txtA").value;
+	var b = document.getElementById("txtB").value;
+	var c = parseInt(a) + parseInt(b);
+	document.getElementById("resultado").innerHTML = c;
+}
