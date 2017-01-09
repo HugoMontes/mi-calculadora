7ba5a3d22b698815ef2d788e242b3f85518dd619
Felix Montes <hugomontes927@gmail.com>
2017-01-09 10:36:46 -0400

    Funcionalidad para los botones restar, multiplicar y dividir

--- calculadora.js
+++ calculadora.js
@@ -5,3 +5,21 @@ function sumar(){
 	var c = parseInt(a) + parseInt(b);
 	document.getElementById("resultado").innerHTML = c;
 }
+function restar(){
+	var a = document.getElementById("txtA").value;
+	var b = document.getElementById("txtB").value;
+	var c = parseInt(a) - parseInt(b);
+	document.getElementById("resultado").innerHTML = c;
+}
+function multiplicar(){
+	var a = document.getElementById("txtA").value;
+	var b = document.getElementById("txtB").value;
+	var c = parseInt(a) * parseInt(b);
+	document.getElementById("resultado").innerHTML = c;
+}
+function dividir(){
+	var a = document.getElementById("txtA").value;
+	var b = document.getElementById("txtB").value;
+	var c = parseInt(a) / parseInt(b);
+	document.getElementById("resultado").innerHTML = c;
+}
