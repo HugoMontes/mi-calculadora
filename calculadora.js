d7611908d581ef8a6928b7296343e68575e6c41e
Felix Montes <hugomontes927@gmail.com>
2017-01-24 10:59:40 -0400

    Se ha agregado la funcionalidad factorial

--- calculadora.js
+++ calculadora.js
@@ -29,3 +29,17 @@ function potencia(){
 	var c = Math.pow(parseInt(a),parseInt(b));
 	document.getElementById("resultado").innerHTML = c;
 }
+function factorial(){
+	var a = parseInt(document.getElementById("txtA").value);
+	var b = parseInt(document.getElementById("txtB").value);
+	var i,fa=1,fb=1;
+	for(i=1;i<=a;i++){
+		fa=fa*i;
+	}
+	for(i=1;i<=b;i++){
+		fb=fb*i;
+	}
+	var c=a+"! = "+fa+"<br/>";
+	c+=b+"! = "+fb+"<br/>";
+	document.getElementById("resultado").innerHTML = c;
+}
